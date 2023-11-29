b23c37c5ed92943eb34c0ddce524968a9bcfbcb1
Alissa S. Cezero <144124273+lissCez@users.noreply.github.com>
2023-11-29 09:30:00 -0300

    Add files via upload

---
+++ script_pagina_inicial.js
@@ -0,0 +1,7 @@
+document.getElementById("verMais").addEventListener("click", () => {
+    document.getElementById("esquerda").classList.toggle("apareceu1");
+});
+
+document.getElementById("botaoChat").addEventListener("click", () => {
+    document.getElementById("direita").classList.toggle("apareceu2");
+});
