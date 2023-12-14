733af84dc178f767e4fd0d645d1f42e7f0e51600
Alissa S. Cezero <144124273+lissCez@users.noreply.github.com>
2023-12-14 11:40:51 -0300

    Delete script_pagina_inicial.js

--- script_pagina_inicial.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("verMais").addEventListener("click", () => {
-    document.getElementById("esquerda").classList.toggle("apareceu1");
-});
-
-document.getElementById("botaoChat").addEventListener("click", () => {
-    document.getElementById("direita").classList.toggle("apareceu2");
-});
